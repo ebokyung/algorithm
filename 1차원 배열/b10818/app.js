c4deeb91f306639a663d8e93aa1d4312646cbd85
ebokyung <90684277+ebokyung@users.noreply.github.com>
2022-09-28 23:52:41 +0900

    1차원 배열 : 최소, 최대

---
+++ 1차원 배열/b10818/app.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+const N = +input[0];
+const testCaseArr = input[1].split(' ').map(i => +i);
+solution(N, testCaseArr);
+
+function solution(N, testCaseArr) {
+   console.log(N, testCaseArr);
+   let min = 1000001;
+   let max = -1000001;
+    for(let i = 0; i < N; i++){
+        let temp = testCaseArr[i];
+        if( temp < min ) min = temp;
+        if( temp > max ) max = temp;
+    }
+    console.log(`${min} ${max}`);
+}
