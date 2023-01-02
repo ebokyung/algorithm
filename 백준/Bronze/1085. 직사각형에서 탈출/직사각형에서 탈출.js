1f6cfb75a77a9bfb899a5566d62d64b174f500a9
ebokyung <90684277+ebokyung@users.noreply.github.com>
2023-01-02 16:56:22 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.js
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.js
@@ -2,25 +2,14 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split(' ');
 
-const testCase = {
-    x: +input[0],
-    y: +input[1],
-    w: +input[2],
-    h: +input[3],
-}
-solution(testCase);
+solution(input);
 
-function solution(testCase) {
-    let answer = "";
-    const heightMinusY = testCase.h - testCase.y;
-    const widthMinusX = testCase.w - testCase.x;
-    const newArr = [testCase.x, testCase.y, heightMinusY, widthMinusX];
-    let min = testCase.x;
-    for(let i=0; i<newArr.length; i++){
-        if(min >= newArr[i]) {
-            min = newArr[i];
-        }
-    }
-    answer = min;
+function solution(input) {
+    const x = +input[0];
+    const y = +input[1];
+    const w = +input[2];
+    const h = +input[3];
+    const newArr = [x, w-x, y, h-y];
+    const answer = Math.min(...newArr);
     console.log(answer);
 }
