6e7f665dde87ccc7be0bba00b3c34810fd948c14
ebokyung <90684277+ebokyung@users.noreply.github.com>
2023-01-02 18:04:28 +0900

    [Bronze III] Title: 세 수, Time: 136 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.js
+++ 백준/Bronze/10817. 세 수/세 수.js
@@ -5,12 +5,5 @@ let input = fs.readFileSync(filePath).toString().trim().split(' ').map(i=>+i);
 solution(input);
 
 function solution(testCase) {
-    const maxIndex = testCase.indexOf(Math.max(...testCase));
-    const minIndex = testCase.indexOf(Math.min(...testCase));
-    for(let i=0; i<testCase.length; i++){
-        if(i !== maxIndex && i !== minIndex) {
-            console.log(testCase[i]);
-            break;
-        }
-    }
+    console.log(testCase.sort((a,b)=> a-b)[1]);
 }
