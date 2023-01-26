88d00af1886874a7ed7ec0b085f7ac8cbaf12af1
ebokyung <90684277+ebokyung@users.noreply.github.com>
2023-01-26 19:03:54 +0900

    [Silver III] Title: Four Squares, Time: 240 ms, Memory: 10024 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.js
+++ 백준/Silver/17626. Four Squares/Four Squares.js
@@ -1,13 +1,21 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let n = require('fs').readFileSync(filePath).toString().trim();
+let input = require('fs').readFileSync(filePath).toString().trim();
 
-const dp = new Array(n+1).fill(0);
-dp[1] = 1;
-for (let i=2; i<=n; i++) {
-    let min = 4;
-    for (let j=1; j*j<=i; j++) {
-        min = Math.min(min, dp[i-j*j]);
+solution(+input);
+
+function solution(n){
+  let memo = new Array(n+1);
+  memo[0]=0;
+  memo[1]=1;
+  for(let i=2; i<=n; i++){
+    memo[i] = 5; //최대 4개의 자연수로 나타내므로 
+  }
+
+  for(let i=2; i<=n; i++){
+    for(let j=1; j*j<=i; j++){
+      memo[i] = Math.min( memo[i] , 1 + memo[i-j*j]);
     }
-    dp[i] = min + 1;
+  }
+
+  console.log(memo[n]);
 }
-console.log(dp[n]);
