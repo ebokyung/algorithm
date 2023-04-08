990394c2db3d37ab42495efff71e8c0988c53353
bokyung_lee <90684277+ebokyung@users.noreply.github.com>
2023-04-08 22:55:44 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,16 +1,13 @@
 function solution(brown, yellow) {
-    let divisor = []
-    
     const total = brown + yellow;
-    for(let i=3; i*i<=total; i++){
-        if(total%i === 0) divisor.push([total/i, i])
-    }
     
-    for(let i=1; i<=divisor.length; i++){
-        let col = divisor[divisor.length-i][0]
-        let row = divisor[divisor.length-i][1]
-        if((col - 2) * (row - 2) === yellow) {
-            return divisor[divisor.length-i];
+    for(let i=3; i*i<=total; i++){
+        if(total%i === 0) { // 약수이고
+            let col = total/i;
+            let row = i;
+            if((col - 2) * (row - 2) === yellow) { // yellow 모양이 맞으면
+                return [col, row];
+            }
         }
     }
 }
