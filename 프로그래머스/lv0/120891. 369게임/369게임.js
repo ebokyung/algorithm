d0f139a7e73fcbe9c09702cfdc839c0f04eca61a
bokyung_lee <90684277+ebokyung@users.noreply.github.com>
2023-04-17 23:16:04 +0900

    [level 0] Title: 369게임, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,6 +1,12 @@
 function solution(order) {
-        const mySet = new Set([3, 6, 9]);
-    return String(order) 
-        .split("")
-        .filter((num) => mySet.has(parseInt(num))).length;
+    // 풀이 1.
+    //     const mySet = new Set([3, 6, 9]);
+    // return String(order) 
+    //     .split("")
+    //     .filter((num) => mySet.has(parseInt(num))).length;
+    
+    // 풀이 2.
+    return (order
+        .toString()
+        .match(/(3|6|9)/g) ?? []).length
 }
