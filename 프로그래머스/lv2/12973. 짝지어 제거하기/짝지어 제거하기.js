b6dfb356ea57fc68744a8e9ece8c714d7b7e616d
bogyeong_lee <90684277+ebokyung@users.noreply.github.com>
2023-05-07 23:39:37 +0900

    [level 2] Title: 짝지어 제거하기, Time: 46.61 ms, Memory: 46.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    let stack = [];
+    stack.push(s[0]);
+    for(let i=1; i<s.length; i++) {
+        if (stack[stack.length - 1] == s[i]){
+            stack.pop()
+        } else {
+            stack.push(s[i])
+        }
+        // console.log(i + ' : ' + stack);   
+    }
+    return stack.length > 0 ? 0 : 1;
+}
