c4b1eccd8ebca4c96818a65cadb0e5024ba7b532
bogyeong.lee <dlqhrud303@naver.com>
2024-01-17 11:21:55 +0900

    Time: 43 ms (96.74%), Space: 42.3 MB (75.34%) - LeetHub

--- LeetCode/Easy/1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
+++ LeetCode/Easy/1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -3,8 +3,7 @@
  * @return {boolean}
  */
 var uniqueOccurrences = function(arr) {
-    const valueCounts = {};
-    arr.forEach((value) => valueCounts[value] = ~~valueCounts[value] + 1);
-    const occurrences = Object.values(valueCounts);
-    return occurrences.length == new Set(occurrences).size
+    const map = new Map();
+    arr.forEach((value) => map.set(value, (map.get(value) || 0) + 1));
+    return map.size === new Set(map.values()).size
 };
