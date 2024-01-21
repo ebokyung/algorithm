807169d3c88dcf919aa5d36af3dead1e5b2c49cc
bogyeong.lee <dlqhrud303@naver.com>
2024-01-21 11:19:44 +0900

    Time: 52 ms (55.94%), Space: 48.5 MB (5.18%) - LeetHub

--- LeetCode/Medium/0198-house-robber/0198-house-robber.js
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.js
@@ -2,8 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
-    let prev = 0, cur = 0
-    nums.forEach(n => cur = Math.max(n + prev, prev = cur))
-    return cur
+var rob = function (nums) {
+    if (nums.length === 1) return nums[0];
+
+    let dp = Array(nums.length + 1).fill(0);
+
+    dp[0] = 0;
+    dp[1] = nums[0];
+
+    for (let i = 2; i < dp.length; ++i) {
+        dp[i] = Math.max((nums[i - 1] + dp[i - 2]), dp[i - 1])
+    }
+
+    return dp[dp.length - 1];
 };
