99a1c9fb36b584ce59acfd8908c0f26a0dd3b4ed
bogyeong.lee <dlqhrud303@naver.com>
2024-01-22 13:25:33 +0900

    Time: 76 ms (40.98%), Space: 61.4 MB (5.25%) - LeetHub

--- LeetCode/Easy/0645-set-mismatch/0645-set-mismatch.js
+++ LeetCode/Easy/0645-set-mismatch/0645-set-mismatch.js
@@ -3,16 +3,16 @@
  * @return {number[]}
  */
 var findErrorNums = function (nums) {
-    const n = nums.length
-    const actual_sum = sum(nums)  // 1 + 2 + 2 + 4 = 9
-    const unique_sum = sum(new Set(nums))  // sum({1, 2, 4}) = 7
-    const expected_sum = n * (n + 1) / 2  // 4 * (5) // 2 = 10
+    let duplicate = 0, missing = 0
+    let n = nums.length;
+    const mp = new Map();
+    while (n > 0) mp.set(n--, 0);
 
-    const duplicate = actual_sum - unique_sum
-    const missing = expected_sum - unique_sum
+    nums.forEach(n => mp.set(n, mp.get(n) - 1))
+    for(let [key, value] of mp) {
+        if(value === 0) missing = key;
+        if(value === -2) duplicate = key
+    }
+
     return [duplicate, missing]
 };
-
-function sum (arr) {
-    return Array.from(arr).reduce((acc, cur) => acc += cur, 0)
-}
