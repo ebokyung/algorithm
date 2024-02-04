449bee73f69456d32f5e8616c40d15c1af7002a0
bogyeong.lee <dlqhrud303@naver.com>
2024-02-05 08:24:44 +0900

    [level 2] Title: 프로세스, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,21 +1,38 @@
 function solution(priorities, location) {
-    const first = Math.max.apply(null, priorities);
-    const firstProcess = priorities.indexOf(first);
-    const newPriorities = [...priorities];
-    newPriorities[location] = priorities[location] * -1;
-    const queue = [...newPriorities.slice(firstProcess), ...newPriorities.slice(0, firstProcess)];
-
-    let nth = 0;
-    while(queue.length){
-        const curProcessPriority = queue.shift();
-        if(Math.abs(curProcessPriority) >= Math.max(...queue.map(i => Math.abs(i)))) {
-            if(curProcessPriority < 0) return nth+1;
-            nth++;
+    let queue = priorities.map((priority, index)=>({
+        priority,
+        isTarget: index === location
+    }));
+    let nth = 0;       
+    while(true){
+        const curProcess = queue.shift();        
+        if(queue.some(process => process.priority > curProcess.priority)) { 
+            queue.push(curProcess); 
             continue;
-        }
-        queue.push(curProcessPriority);         
+        }                   
+        nth++;
+        if(curProcess.isTarget) return nth;
     }
 }
+
+// function solution(priorities, location) {
+//     const first = Math.max.apply(null, priorities);
+//     const firstProcess = priorities.indexOf(first);
+//     const newPriorities = [...priorities];
+//     newPriorities[location] = priorities[location] * -1;
+//     const queue = [...newPriorities.slice(firstProcess), ...newPriorities.slice(0, firstProcess)];
+
+//     let nth = 0;
+//     while(queue.length){
+//         const curProcessPriority = queue.shift();
+//         if(Math.abs(curProcessPriority) >= Math.max(...queue.map(i => Math.abs(i)))) {
+//             if(curProcessPriority < 0) return nth+1;
+//             nth++;
+//             continue;
+//         }
+//         queue.push(curProcessPriority);         
+//     }
+// }
 
 // ['1', 1, 9, 1, 2, 1], 0
 // [9, 1, 2, 1, '1', 1] // 1
