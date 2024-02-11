50d84d705398b8c3308d5c76e6c6bc3738dbbd6d
bogyeong.lee <dlqhrud303@naver.com>
2024-02-11 23:07:29 +0900

    [level 1] Title: [1차] 비밀지도, Time: 3.46 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,8 +1,17 @@
+// - 10진수로 바로 비트 연산해도 동일하다는 점
+// - toString(2)로 2진수로 변환
+// - replace와 표현식으로 값 바로 바꾸기
+// - 자릿수 맞추는 방법
+//   1. padStart() 메서드
+//   2. '0'.repeat() 이용하기
+
 function solution(n, arr1, arr2) {
     const answer = []
     const map1 = arr1.map((num) => changeDecimalToBinary(num));
     const map2 = arr2.map((num) => changeDecimalToBinary(num));
     for(let i=0; i<n; i++){
+        console.log(arr1[i] | arr2[i], (arr1[i] | arr2[i]).toString(2))
+        console.log(arr1[i].toString(2), arr2[i].toString(2))
         const decode = map1[i].split('').map((val, index) => {
             return val | map2[i][index] ? '#': ' '
         });
@@ -20,3 +29,15 @@ function solution(n, arr1, arr2) {
         return temp.join('');
     }
 }
+
+
+// 다른 풀이 1.
+// function solution(n, arr1, arr2) {
+//     return arr1.map((v, i) => addZero(n, (v | arr2[i]).toString(2)).replace(/1|0/g, a => +a ? '#' : ' '));
+// }
+// const addZero = (n, s) => {
+//     return '0'.repeat(n - s.length) + s;
+// }
+
+// 다른 풀이 2.
+// var solution=(n,a,b)=>a.map((a,i)=>(a|b[i]).toString(2).padStart(n,0).replace(/0/g,' ').replace(/1/g,'#'))
