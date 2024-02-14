bddc97995b0ba1a2986c6ab78185299bbc2bada3
bogyeong.lee <dlqhrud303@naver.com>
2024-02-14 14:14:20 +0900

    [Silver II] Title: 유기농 배추, Time: 308 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.js
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -1,57 +1,38 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-// 상 하 좌 우
-const ds = [[-1, 0], [1, 0], [0, 1], [0, -1]];
-let graph, xMax, yMax, c;
-let tc = Number(input.shift());
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
-// 삽입 지렁이 개수 확인
-const putWorms = () => {
-  let result = 0;
-  for(let i = 0 ; i < yMax ; i++){
-    for(let j = 0 ; j < xMax ; j++){
-      if(graph[i][j]) {
-        bfs(i, j);
-        result++;
-      }
-    }
-  }
-  console.log(result);
+const directions = [[-1, 0],[1, 0],[0, 1],[0, -1]];
+
+const testcase = +input.shift();
+
+for (let i = 0; i < testcase; i++) {
+  const [M, N, NumberOfCabbage] = input.shift().split(" ").map(Number);
+  const cabbages = input.slice(0, NumberOfCabbage);
+
+  console.log(getEarthWormCnt(M, N, cabbages));
+    
+  input = input.slice(NumberOfCabbage); // next test case
 }
 
-// 반복
-const bfs = (startX, startY) => {
-  const queue = [[startX, startY]];
-  while(queue.length){
-    const [x, y] = queue.shift();
-    if(!graph[x][y]) continue;
-    else graph[x][y] = 0;
+function getEarthWormCnt(M, N, cabbages) {
+  let earthWorm = 0;
+  const visited = Array.from({ length: N }, () => new Array(M).fill(0));
 
-    for(let i = 0; i < 4; i++){
-      const xPos = x + ds[i][0];
-      const yPos = y + ds[i][1];
-
-      if(xPos < 0 || yPos < 0 || xPos >= yMax || yPos >= xMax) continue;
-      if(graph[xPos][yPos]) queue.push([xPos, yPos]);
+  const dfs = (y, x) => {
+    if (x < 0 || y < 0 || x >= M || y >= N) return;
+    if (!cabbages.includes(x + " " + y)) return;
+    if (visited[y][x]) return;
+    visited[y][x] = 1;
+    for (let [dy, dx] of directions) {
+      dfs(y + dy, x + dx);
     }
-  }
-};
+  };
 
-for(let i = 0 ; i < tc ; i++){
-  [xMax, yMax, c] = input.shift().split(' ').map(Number);
-
-  // 그래프 생성
-  graph = [...Array(yMax)].map(e => Array(xMax).fill(0));
-
-  // 배추 삽입
-  for(let j = 0 ; j < c; j++){
-    let [cx, cy] = input[j].split(' ').map(Number);
-    graph[cy][cx] = 1;
+  for (let i = 0; i < cabbages.length; i++) {
+    const [x, y] = cabbages[i].split(" ").map(Number);
+    if (visited[y][x] == 1) continue;
+    earthWorm++;
+    dfs(y, x);
   }
 
-  // 배추흰지렁이 개수 확인
-  putWorms();
-
-  // next test case
-  input = input.slice(c);
+  return earthWorm;
 }
