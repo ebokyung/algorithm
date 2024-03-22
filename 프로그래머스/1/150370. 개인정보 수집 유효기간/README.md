7a42c842dc2f216f3b2489b20adc9e15a028f5d3
bogyeong.lee <dlqhrud303@naver.com>
2024-03-22 18:09:51 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.51 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
@@ -1,10 +1,10 @@
 # [level 1] 개인정보 수집 유효기간 - 150370 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370#) 
 
 ### 성능 요약
 
-메모리: 34.1 MB, 시간: 0.86 ms
+메모리: 33.6 MB, 시간: 0.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 22일 18:08:41
+2024년 03월 22일 18:09:49
 
 ### 문제 설명
 
--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,44 +1,21 @@
 function solution(today, terms, privacies) {
-    const todayDate = new Date(today)
+  var answer = [];
     
-    const termsTable = {}
-    terms.forEach(term => {
-        const [key, value] = term.split(' ')
-        termsTable[key] = +value
-    })
+  var [year, month, date] = today.split(".").map(Number);
+  var todates = year * 12 * 28 + month * 28 + date;
     
-    const result = []
-    privacies.forEach((privacy, idx) => {
-        const [startDateStr, termName] = privacy.split(' ')
-        const endDate = getEndDate(startDateStr, termsTable[termName])
-        if(todayDate - endDate > 0) result.push(idx+1)
-    })
-    return result
-}
+  var t = {};
+  terms.forEach((e) => {
+    let [a, b] = e.split(" ");
+    t[a] = Number(b);
+  });
+    
+  privacies.forEach((e, i) => {
+    var [day, term] = e.split(" ");
+    day = day.split(".").map(Number);
+    var dates = day[0] * 12 * 28 + day[1] * 28 + day[2] + t[term] * 28;
+    if (dates <= todates) answer.push(i + 1);
+  });
 
-
-function getEndDate(startDate, m) {
-    let [yyyy, mm, dd] = startDate.split('.').map(Number)
-    
-    if(dd - 1 === 0) {
-        dd = 28
-        mm -= 1
-    } else {
-        dd -= 1
-    }
-    
-    if(mm + m > 12) {
-        let temp = mm + m;
-        if(temp % 12 === 0){
-            mm = 12
-            yyyy += Math.floor((temp - 1) / 12)
-        } else {
-            mm = temp % 12
-            yyyy += Math.floor(temp / 12)
-        }
-    } else {
-        mm += m
-    }
-
-    return new Date([yyyy, mm, dd].join('.'));
+  return answer;
 }
