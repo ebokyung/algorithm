cd2cac3f4c63b9615e96c43081d1dcfbef1a3792
bogyeong.lee <dlqhrud303@naver.com>
2024-03-28 10:16:49 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,16 +1,8 @@
 function solution(food) {
-    var answer = '';
-    for(let i=1; i<food.length; i++){
-        for(let j=1; j<=Math.floor(food[i]/2); j++){
-            answer += i
-        }
+    let res = '';
+    for (let i = 1; i < food.length; i++) {
+        res += String(i).repeat(Math.floor(food[i]/2));
     }
-    return answer + 0 + reverse(answer);
-}
 
-function reverse (str) {
-    let result = '';
-    const string = str.split('')
-    for(let i=0; i<str.length; i++) result += string.pop()
-    return result;
+    return res + '0' + [...res].reverse().join('');
 }
