9ed0fb3ef784c344ed83bab3965897227ccb8d87
bogyeong.lee <dlqhrud303@naver.com>
2024-05-27 16:16:00 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.59 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,24 +1,44 @@
+// function solution(survey, choices) {
+//     var answer = '';
+//     let scores = [3,2,1,0,1,2,3]
+//     let mbti = new Map();
+    
+//     for(let i=0; i<survey.length; i++){
+//         if(choices[i] === 4) continue;
+        
+//         let typeIdx = 0;
+//         if(choices[i] > 4) typeIdx = 1;
+        
+//         let score = scores[choices[i] - 1];
+        
+//         let type = survey[i].split('')[typeIdx];
+//         mbti.set(type, mbti.get(type) + score || score)
+//     }
+    
+//     let types = [['R', 'T'], ['C','F'], ['J','M'], ['A','N']]
+//     for(let [a,b] of types){
+//         answer += (mbti.get(a) || 0) >= (mbti.get(b) || 0) ? a : b
+//     }
+    
+//     return answer;
+// }
+
 function solution(survey, choices) {
-    var answer = '';
-    let scores = [3,2,1,0,1,2,3]
-    let mbti = new Map();
-    
-    for(let i=0; i<survey.length; i++){
-        if(choices[i] === 4) continue;
-        
-        let typeIdx = 0;
-        if(choices[i] > 4) typeIdx = 1;
-        
-        let score = scores[choices[i] - 1];
-        
-        let type = survey[i].split('')[typeIdx];
-        mbti.set(type, mbti.get(type) + score || score)
-    }
-    
-    let types = [['R', 'T'], ['C','F'], ['J','M'], ['A','N']]
-    for(let [a,b] of types){
-        answer += (mbti.get(a) || 0) >= (mbti.get(b) || 0) ? a : b
-    }
-    
-    return answer;
+    const MBTI = {};
+    const types = ["RT","CF","JM","AN"];
+
+    // mbti 유형별 점수 초기화
+    types.forEach((type) =>
+        type.split('').forEach((char) => MBTI[char] = 0)
+    )
+
+    choices.forEach((choice, index) => {
+        const [disagree, agree] = survey[index];
+
+        MBTI[choice > 4 ? agree : disagree] += Math.abs(choice - 4);
+        // choice === 4 경우는 어차피 0이 더해져 상관없음
+        // 1~7 -> -3~3 -> 절대값 0~3
+    });
+
+    return types.map(([a, b]) => MBTI[b] > MBTI[a] ? b : a).join("");
 }
