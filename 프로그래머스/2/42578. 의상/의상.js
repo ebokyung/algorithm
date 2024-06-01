3c07a9e7c13cfe8567a2bcd348c0288ea79e8302
bogyeong.lee <dlqhrud303@naver.com>
2024-06-01 15:55:52 +0900

    [level 2] Title: 의상, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -16,3 +16,17 @@ function solution(clothes) {
     
     return answer - 1;
 }
+
+/** +1은 그 옷을 안입는 경우, 마지막 -1은 모든 옷을 안입는 경우 제외를 위한 것
+** 만약 모자 2종류, 상의 2종류면 
+** (모자1, 모자2, 모자 안쓰기)*(상의1, 상의2, 상의 안입기)=9인데 
+** (모자 안쓰기, 상의 안입기) 1개 빼면 9-1=8
+*/
+
+// 짧은 풀이
+// function solution(clothes) {
+//     return Object.values(clothes.reduce((obj, t)=> {
+//         obj[t[1]] = obj[t[1]] ? obj[t[1]] + 1 : 1;
+//         return obj;
+//     } , {})).reduce((a,b)=> a*(b+1), 1)-1;    
+// }
