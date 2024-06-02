7a6786857f833a6ebe38c771b05ffeafa95bf731
bogyeong.lee <dlqhrud303@naver.com>
2024-06-03 00:00:06 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -13,13 +13,36 @@ function solution(progresses, speeds) {
     return result
 }
 
-// Math.ceil((100 - progresses) / speeds)
-// [7, 3, 9]
-// [0, -4, 2]
+// // Math.ceil((100 - progresses) / speeds)
+// // [7, 3, 9]
+// // [0, -4, 2]
 
 
-// [5, 10, 1, 1, 20, 1]
-// [0, 5, -4, -4, 15, -4]
-// [ , 0, -9, -9, 10, -9]
-// [ ,  ,  0,  0, 19, 0]
-// [ ,  ,   ,   ,  0, -19]
+// // [5, 10, 1, 1, 20, 1]
+// // [0, 5, -4, -4, 15, -4]
+// // [ , 0, -9, -9, 10, -9]
+// // [ ,  ,  0,  0, 19, 0]
+// // [ ,  ,   ,   ,  0, -19]
+
+// function solution(progresses, speeds){
+//     var answer = []
+    
+//     const remainTasks = progresses.map((progress, index) => Math.ceil((100-progress)/speeds[index]))
+//     console.log(remainTasks)
+    
+//     while(remainTasks.length){
+//         const task = remainTasks.shift();
+//         let acc = 1;
+        
+//         const doneTask = remainTasks.map(t => t-task)
+//         for(let t of doneTask){
+//             if(t < 0) acc++;
+//             else {
+//                 answer.push(acc)
+//                 break;
+//             }
+//         }
+//     }
+    
+//     return answer;
+// }
