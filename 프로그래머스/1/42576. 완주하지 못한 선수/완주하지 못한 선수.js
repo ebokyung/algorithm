aa9d7192e5a46e5feed41c7ecb053a23153f9bd9
bogyeong.lee <dlqhrud303@naver.com>
2024-06-05 19:44:58 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 52.17 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,12 +1,30 @@
+// function solution(participant, completion) {
+//     const hash = new Map();
+//     participant.forEach(p => {
+//         hash.set(p, hash.get(p) + 1 || 1)
+//     })
+//     completion.forEach(c => {
+//         hash.set(c, hash.get(c) - 1)
+//     })
+//     for(let person of hash.entries()){
+//         if(person[1] > 0) return person[0]
+//     }
+// }
+
 function solution(participant, completion) {
-    const hash = new Map();
-    participant.forEach(p => {
-        hash.set(p, hash.get(p) + 1 || 1)
-    })
-    completion.forEach(c => {
-        hash.set(c, hash.get(c) - 1)
-    })
-    for(let person of hash.entries()){
-        if(person[1] > 0) return person[0]
+    const map = new Map();
+
+    for(let i = 0; i < participant.length; i++) {
+        let a = participant[i], 
+            b = completion[i]; // undefined
+
+        map.set(a, (map.get(a) || 0) + 1);
+        map.set(b, (map.get(b) || 0) - 1);
     }
+
+    for(let [k, v] of map) {
+        if(v > 0) return k;
+    }
+
+    return 'nothing';
 }
