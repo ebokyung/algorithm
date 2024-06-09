2c337d3448a2b2915e35724355ddfb27f0afc9d5
bogyeong.lee <dlqhrud303@naver.com>
2024-06-09 13:49:54 +0900

    [Silver I] Title: 봄버맨, Time: 180 ms, Memory: 12440 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.js
+++ 백준/Silver/16918. 봄버맨/봄버맨.js
@@ -1,42 +1,57 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
-let [R,C,N] = input[0].split(' ').map(i => +i)
-const bombs = input.slice(1).map(i => {
-  let temp = i.split('')
-  return temp.map(bomb => bomb === 'O' ? 3 : 0)
-})
+const [r, c, n] = input[0].split(" ").map(Number);
+const map = input.slice(1).map(i => i.split(''))
 
-const directions = [[0,-1], [0,1], [1,0], [-1,0]]
+// 상하좌우 탐색을 위한 방향
+const dir = [[-1, 0],[1, 0],[0, -1],[0, 1]];
 
-let time = 1;
-while(time <= N){
-  if(time % 2 === 0){ // 짝수 시간에 폭탄 채우기
-    for(let i=0; i<R; i++){
-      for(let j=0; j<C; j++){
-        if(bombs[i][j] === 0) bombs[i][j] = 3;
-        else bombs[i][j] -= 1;
-      }
-    }
-  } else { // 홀수 시간에 폭발
-    for(let i=0; i<R; i++){
-      for(let j=0; j<C; j++){
-        if(bombs[i][j] === 1) {
-          bombs[i][j] = 0;
-          for(let [dy,dx] of directions){
-            const ny = i + dy;
-            const nx = j + dx;
-            if(ny >= R || nx >= C || ny < 0 || nx <0) continue;
-            if(bombs[ny][nx] > 1) bombs[ny][nx] = 0
+// 맵 출력 함수
+function printMap(arr) {
+  console.log(arr.map(v => v.join("")).join("\n"));
+}
+
+// 폭탄 함수
+function bomb(arr) {
+  const map = Array.from({ length: r }, () => Array(c).fill("O"));
+  for (let i = 0; i < r; i++) {
+    for (let j = 0; j < c; j++) {
+      if (arr[i][j] === "O") {
+        map[i][j] = ".";
+        for (let [dy, dx] of dir) {
+          let nr = i + dy;
+          let nc = j + dx;
+          if (nr >= 0 && nr < r && nc >= 0 && nc < c) {
+            map[nr][nc] = ".";
           }
-        } else if (bombs[i][j] > 1) bombs[i][j] -= 1;
+        }
       }
     }
   }
-  time++;
+  return map;
 }
 
-let answer = ''
-for(let i=0; i<R; i++){
-  answer += bombs[i].map(bomb => bomb === 0 ? '.' : 'O').join('') + '\n'
+if (n === 1) { 
+  printMap(map); // 초기 상태를 출력
+} else if (n % 2 === 0) { 
+  // 모든 칸에 폭탄이 설치된 상태를 출력
+  printMap(Array.from({ length: r }, () => Array(c).fill("O"))); 
+} else if (n % 4 === 1) {
+  printMap(bomb(bomb(map))); // 두 번째 폭발 상태를 출력
+} else if (n % 4 === 3) {
+  printMap(bomb(map)); // 첫 번째 폭발 상태를 출력
 }
-console.log(answer)
+
+/**
+ * 1. 초기 상태
+ * t = 0: 초기 상태
+ * 2. 첫 번째 폭발 
+ * t = 3: 초기 상태에서 3초 후 폭발한 상태
+ * 3. 두 번째 폭발 
+ * t = 5: 초기 상태에서 5초 후 폭발한 상태 (첫 번째 폭발 이후 2초 후 설치된 폭탄들이 폭발)
+ * 
+ * 이 패턴이 4초 주기로 반복되기 때문에 다음과 같은 규칙을 발견할 수 있습니다:
+ * 
+ * t % 4 == 1: 두 번째 폭발 상태 (5초 후, 9초 후, ...)
+ * t % 4 == 3: 첫 번째 폭발 상태 (3초 후, 7초 후, ...)
+ */
