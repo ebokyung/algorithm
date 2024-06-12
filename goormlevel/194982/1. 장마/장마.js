4fa24075b30b080e633f46d267b5704fe0f34d96
bogyeong.lee <dlqhrud303@naver.com>
2024-06-12 15:46:20 +0900

    [goormlevel] Title: 장마, Time: 1.09 ms, Memory: 44.29 MB, Difficulty: 2 -BaekjoonHub

---
+++ goormlevel/194982/1. 장마/장마.js
@@ -0,0 +1,35 @@
+// Run by Node.js
+const readline = require('readline');
+
+(async () => {
+	let rl = readline.createInterface({ input: process.stdin });
+	
+	let N, M, houses, rainySeason = [];
+	for await (const line of rl) {
+		if(!N && !M) [N,M] = line.split(' ').map(Number);
+		else if (!houses) houses = line.split(' ').map(Number);
+		else rainySeason.push(line.split(' ').map(Number))
+		if(rainySeason.length === M) rl.close()
+	}
+	
+	solution(N, M, houses, rainySeason)
+	process.exit();
+})();
+
+function solution (N, M, houses, rainySeasons) {
+	let rained = new Set()
+	for(let i=0; i<M; i++){
+		const [s,e] = rainySeasons[i];
+		for(let j=s-1; j<e; j++){
+			houses[j]++;
+			rained.add(j)
+		}
+		if((i+1) % 3 === 0){
+			for(let houseIdx of rained){
+				houses[houseIdx]--;
+			}
+			rained.clear()
+		}
+	}
+	console.log(houses.join(' '))
+}
