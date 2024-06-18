d98222083c48595da8d3b9bca84558e94ba5708f
bogyeong.lee <dlqhrud303@naver.com>
2024-06-18 21:35:20 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -1,21 +1,30 @@
+// 재귀 풀이
+// min(밑으로, 위로 올라갔다 밑으로)
 function solution(storey) {
-    var answer = 0;
-    
-    while(storey != 0){
-        const temp = storey % 10;
-        
-        if(temp >= 6) {
-            answer += 10 - temp; 
-            storey += 10 - temp; 
-        } else if (temp == 5 && (~~(storey / 10)) % 10 >= 5){
-            answer += 10 - temp;
-            storey += 10 - temp;
-        } else { // temp < 5 || (temp == 5 && (storey / 10) % 10 < 5)
-            answer += temp; 
-        }
-        
-        storey = Math.floor(storey / 10);
-    }
-    
-    return answer;
+    if (storey < 5) return storey;
+    const r = storey % 10;
+    const m = (storey - r) / 10;
+    return Math.min(r + solution(m), 10 - r + solution(m + 1));
 }
+
+// function solution(storey) {
+//     var answer = 0;
+    
+//     while(storey != 0){
+//         const temp = storey % 10;
+        
+//         if(temp >= 6) {
+//             answer += 10 - temp; 
+//             storey += 10 - temp; 
+//         } else if (temp == 5 && (~~(storey / 10)) % 10 >= 5){
+//             answer += 10 - temp;
+//             storey += 10 - temp;
+//         } else { // temp < 5 || (temp == 5 && (storey / 10) % 10 < 5)
+//             answer += temp; 
+//         }
+        
+//         storey = Math.floor(storey / 10);
+//     }
+    
+//     return answer;
+// }
