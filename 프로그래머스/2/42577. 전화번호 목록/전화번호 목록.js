01cd04ba675937d61185dc04f442f25a9606083d
bogyeong.lee <dlqhrud303@naver.com>
2024-06-27 16:36:51 +0900

    [level 2] Title: 전화번호 목록, Time: 232.16 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,39 +1,40 @@
-
-
-// 풀이 1.
-// // index 접근이 아니라 key로 바로 접근하는 것이 효율성 문제의 핵심!
-// function solution(phone_book) {
-//     var answer = true;
+// 1. 내 접두어를 가진 단어가 있는지
+function solution(phone_book){
+    let answer = true;
     
-//     const map = new Map();
-//     phone_book.forEach(phone_number => {
-//         map.set(phone_number, true)
-//     })
+    const map = new Map();
     
-//     for(let i=0; i<phone_book.length; i++){
-//         let cur_number = phone_book[i];
-//         let temp = ''
-//         for(let j=0; j<cur_number.length; j++){
-//             temp += cur_number[j]
-//             if(map.get(temp) && temp !== cur_number){
-//                 answer = false;
-//                 break;
-//             }
+    for(let phone_number of phone_book){
+        map.set(phone_number, true)
+    }
+    
+    for(let phone_number of phone_book){
+        let cur_prefix = '';
+        for(let i=0; i<phone_number.length; i++){
+            cur_prefix += phone_number[i];
+            if(map.get(cur_prefix) && cur_prefix !== phone_number){
+                answer = false;
+                break;
+            }
+        }
+        if(!answer) break;
+    }
+    
+    return answer;
+}
+
+// 2. 내가 다른 단어의 접두어인지
+// function solution(phone_book){
+//     let answer = true;
+    
+//     phone_book.sort((a,b) => a < b ? -1 : 1)
+    
+//     for(let i=0; i<phone_book.length - 1; i++){
+//         if(phone_book[i+1].startsWith(phone_book[i])){
+//             answer = false;
+//             break;
 //         }
-//         if(!answer) break;
 //     }
     
 //     return answer;
 // }
-
-// 풀이 2.
-// 정렬***해서 매칭
-function solution(phone_book) {
-    // sort - 기본 정렬 순서는 문자열의 유니코드 코드 
-    // Array.prototype.some() - 적어도 하나라도 통과하는지
-    // String.prototype.startsWith(searchString[, position])
-    return !phone_book.sort().some((_num,i)=> {
-        if(i === phone_book.length -1) return false;
-        return phone_book[i+1].startsWith(phone_book[i]);        
-    })
-}
