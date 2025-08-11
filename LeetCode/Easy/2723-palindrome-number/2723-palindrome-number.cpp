2bca0b6a95bdffea4fb6f8aecfe348e64a1f07aa
bogyeong.lee <dlqhrud303@naver.com>
2025-08-11 23:16:20 +0900

    Time: 804 ms (5.01%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Easy/2723-palindrome-number/2723-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        int div = 1;
+        while (x / div >= 10) {
+            div *= 10;
+        }
+        while (x > 0) {
+            int high = x / div;
+            int low = x % 10;
+            if (high != low) return false;
+            x = (x % div) / 10;
+            div = div / 100;
+        }
+        return true;
+    }
+};
